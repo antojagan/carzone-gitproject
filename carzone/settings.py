e3ff5ff11405848dd49a2b9ef5b00e7c6bf1410d
antojagan <antoc36u@gmail.com>
2023-02-25 14:03:36 +0530

    describe

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'anto',
+        'HOST': 'localhost',
     }
 }
 
@@ -122,6 +125,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'carzone/static',
 ]
 
+# Media Settings
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
